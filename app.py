3162c0cbbbd088a3654ff86d07f06f514a75e0ac
Fateme Zamanian <fatemehzamanian.1999@gmail.com>
2022-08-08 21:01:57 +0430

    first

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+
+
+app = Flask(__name__)
+
+#route for bind url to functions
+#index
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/resume')
+def resume():
+    return redirect("https://resume.io/r/jav1DVI6t")
+
+
+@app.route('/github')
+def github():
+    return redirect("https://github.com/FatemeZamanian")
+
+
+@app.route('/insta')
+def insta():
+    return redirect("https://instagram.com/python.with.fateme?igshid=YmMyMTA2M2Y=")
+
+@app.route('/linkedin')
+def linkedin():
+    return redirect("https://www.linkedin.com/in/fatemezamanian")
+
+@app.route('/youtube')
+def youtube():
+    return redirect("https://youtube.com/channel/UCfI5V_TExc9ntvGSdUNjJ4Q")
+
---
+++ static/st.js
@@ -0,0 +1,6 @@
+let btn0=document.getElementById("lbtn0");
+
+btn0.onmouseenter=function(){
+    // btns0.style.display="block";
+    btns0.style.background = "url('static/github.gif')";
+};
