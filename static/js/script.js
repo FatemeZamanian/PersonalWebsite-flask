0d42babd6707c205bc08b159bf057e38433f31f4
Fateme Zamanian <fatemehzamanian.1999@gmail.com>
2022-08-18 01:50:46 +0430

    update folders

--- static/js/script.js
+++ static/js/script.js
@@ -14,33 +14,33 @@ resbtn.onmouseleave=function(){
 
 
 btns[0].onmouseenter=function(){
-    btns[0].style.background = "url('static/github.gif')";
+    btns[0].style.background = "url('static/images/github.gif')";
 };
 
 btns[0].onmouseleave=function(){
-    btns[0].style.background = "url('static/github.png')";
+    btns[0].style.background = "url('static/images/github.png')";
 };
 
 btns[1].onmouseenter=function(){
-    btns[1].style.background = "url('static/youtube.gif')";
+    btns[1].style.background = "url('static/images/youtube.gif')";
 };
 
 btns[1].onmouseleave=function(){
-    btns[1].style.background = "url('static/youtube.png')";
+    btns[1].style.background = "url('static/images/youtube.png')";
 };
 
 btns[2].onmouseenter=function(){
-    btns[2].style.background = "url('static/insta.gif')";
+    btns[2].style.background = "url('static/images/insta.gif')";
 };
 
 btns[2].onmouseleave=function(){
-    btns[2].style.background = "url('static/insta.png')";
+    btns[2].style.background = "url('static/images/insta.png')";
 };
 
 btns[3].onmouseenter=function(){
-    btns[3].style.background = "url('static/linkedin.gif')";
+    btns[3].style.background = "url('static/images/linkedin.gif')";
 };
 
 btns[3].onmouseleave=function(){
-    btns[3].style.background = "url('static/linkedin.png')";
+    btns[3].style.background = "url('static/images/linkedin.png')";
 };
