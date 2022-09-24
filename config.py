cd17447bff60b23714208b9164c40dc554dbeca2
Fateme Zamanian <fatemehzamanian.1999@gmail.com>
2022-09-25 00:27:09 +0330

    blogs

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
 
-database_host = "fatemezamanian-db"
-database_user = "root"
-database_password = ""
-database_name = ""
+class Config:    
+    SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS=os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS',False)
 
-database_url = "sqlite:///database.db"
-upload_folder = "./static/"
+class Development(Config):
+    DEBUG=True
+
+class Production(Config):
+    DEBUG=False
